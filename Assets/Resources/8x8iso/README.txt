1a93f31a5aab2158f7ebc84a37c42fa1fa104f20
Stanis Kapusta <51389578+TNtube@users.noreply.github.com>
2023-04-01 10:53:45 +0200

    Add dotween

---
+++ Assets/Resources/8x8iso/README.txt
@@ -0,0 +1,10 @@
+Hi, I'm happysmily!
+Welcome to my 8 x 8 isometric tileset! This was an experimental tileset to see how small I could make something so thank you for picking this up!
+
+Feel free to use this for commercial & non commercial products.
+Please edit these to your needs, no credit necessary - although appreciated!
+
+Find me on Twitter @happysmilu
+or find more samples at https://happysmily.itch.io/
+
+Good luck with whatever you're doing, thanks again!
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
+    public float moveSpeed;
+    public float zoomSpeed;
+    public float minZoomDist;
+    public float maxZoomDist;
+    
+    private Vector3 _lastMousePos;
+
+    private Camera _cam;
+    bool IsMouseOverGameWindow => 
+        !(0 > Input.mousePosition.x
+          || 0 > Input.mousePosition.y
+          || Screen.width < Input.mousePosition.x
+          || Screen.height < Input.mousePosition.y);
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Zoom();
+        Move();
+    }
+
+    void Zoom()
+    {
+        if (!IsMouseOverGameWindow) return;
+        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
+        _cam.orthographicSize += -scrollInput * zoomSpeed * Time.deltaTime;
+    }
+    
+    void Move()
+    {
+        if (!Input.GetMouseButton(1)) return;
         
+        Vector3 delta = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0.0f);
+        
+        Vector3 dir = -delta;
+        transform.position += dir * moveSpeed * Time.deltaTime;
     }
 }
